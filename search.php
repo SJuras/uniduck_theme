f87abc997626c2029c554081c6185a5fe93a78b8
SJuras <jurasstjepko@gmail.com>
2020-06-08 18:53:03 +0200

    Fixing fonts

---
+++ search.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Search Page
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Blog Site Template</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Blog Site Template">
+    <meta name="author" content="https://youtube.com/FollowAndrew">
+    <link rel="shortcut icon" href="images/logo.png">
+
+    <?php wp_head(); ?>
+
+  </head>
+
+  <body>
+
+
+    <div>
+      <?php get_search_form(); ?>
+    </div>
+
+
+
+
+  <?php get_footer(); ?>
