56faf6824d01f2f1a100889bb8ab0a8b0e08cb3d
SJuras <jurasstjepko@gmail.com>
2020-06-08 17:30:29 +0200

    Added more into functions.php

--- functions.php
+++ functions.php
@@ -70,10 +70,7 @@ function show_tags()
 
 // shortcodes
 
-function someone_shortcode(){
-  return '<p class="someone_sc">Someone saw a white creature <br /> with the looks of a horse, yet <br /> having a beautiful horn... </p>';
-}
-add_shortcode('someone_sc', 'someone_shortcode');
+
 
 
 function everyone_shortcode(){
