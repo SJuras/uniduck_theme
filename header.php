8e51d5feab7a27d6af38c64c87a08359124d9fd8
SJuras <jurasstjepko@gmail.com>
2020-06-08 22:55:19 +0200

    css home page

--- header.php
+++ header.php
@@ -1,21 +1,26 @@
 <header>
   <div class="container">
-    <div class="logo">
+
+
+    <nav class="navbar navbar-expand-lg">
       <?php
       if ( function_exists( 'the_custom_logo' ) ) {
        the_custom_logo();
       }
       ?>
-    </div>
-    <div class="main-menu">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <?php
         wp_nav_menu(
           array(
             'theme-location' => 'top-menu',
-            'menu_class' => 'top-bar',
+            'menu_class' => 'navbar-nav mr-auto',
           )
         );
       ?>
     </div>
+    </nav>
   </div>
 </header>
